15cb2e24a895c78308685a1e1a7c5c4b16bfc0d1
Syed Rafay <syedrafay76432@gmail.com>
2024-01-01 03:18:29 +0500

    remove index.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
-const express = require("express")
-require("./db/mongoose")
-const userRouter = require("./routers/users")
-const taskRouter = require("./routers/tasks")
-const auth = require("./middleware/auth")
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/users");
+const taskRouter = require("./routers/tasks");
+const auth = require("./middleware/auth");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())//automatically parse incomming json data in to object
-app.use(userRouter)//register router
-app.use(taskRouter)//register router
+app.use(express.json()); //automatically parse incomming json data in to object
+app.use(userRouter); //register router
+app.use(taskRouter); //register router
 
-module.exports = app
+const port = process.env.PORT;
 
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
