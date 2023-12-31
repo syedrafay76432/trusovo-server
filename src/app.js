ff056734747dfd835c404a90b4a3e09329a72808
Syed Rafay <syedrafay76432@gmail.com>
2024-01-01 03:25:50 +0500

    remove index.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const taskRouter = require("./routers/tasks");
 const auth = require("./middleware/auth");
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(express.json()); //automatically parse incomming json data in to object
 app.use(userRouter); //register router
